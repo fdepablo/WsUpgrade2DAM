21cfaa434004e50620164cf06dfc7f79920931c4
Dani Costas <145155375+danicostas-xyz@users.noreply.github.com>
2024-10-08 12:48:40 +0200

    Fixed bug in InterfazUsuario that prevented user login. Minor cosmetic fixes in other classes.

--- AD_ACT_01_Fichero/src/modelo/entidad/Usuario.java
+++ AD_ACT_01_Fichero/src/modelo/entidad/Usuario.java
@@ -5,24 +5,28 @@ import java.util.Objects;
 public class Usuario {
 	private String nombre;
 	private String password;
-	
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(nombre, password);
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -34,11 +38,10 @@ public class Usuario {
 		Usuario other = (Usuario) obj;
 		return Objects.equals(nombre, other.nombre) && Objects.equals(password, other.password);
 	}
-	
+
 	@Override
 	public String toString() {
 		return nombre + "/" + password;
 	}
-	
-	
+
 }
